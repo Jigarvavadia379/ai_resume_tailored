d59874ce28213128095acf4eefd1251770f207a1
Jigarvavadia379 <jigar.vavadia@gmail.com>
2025-06-20 15:11:10 +0530

    Files Added

---
+++ pages/_app.tsx
@@ -0,0 +1,6 @@
+import '../styles/globals.css'; // adjust this path if needed
+import type { AppProps } from 'next/app';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
